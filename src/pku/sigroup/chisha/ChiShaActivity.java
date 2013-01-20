e36f122d28cf692cc7b98fc4d61034e1780df583
Kevin Tong <logicmd@gmail.com>
2013-01-20 16:30:08 +0800

    refactor to use a shake handler

--- src/pku/sigroup/chisha/ChiShaActivity.java
+++ src/pku/sigroup/chisha/ChiShaActivity.java
@@ -2,37 +2,41 @@
 
 package pku.sigroup.chisha;
 
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.Vibrator;
 import android.app.Activity;
-import android.app.Service;
+import android.content.Context;
 import android.view.Menu;
 import android.widget.TextView;
 
-public class ChiShaActivity extends Activity implements SensorEventListener {
+public class ChiShaActivity extends Activity {
+	private ShakeListener mShaker;
+	String[] canteen;
 	TextView text;
-    //定义sensor管理器
-	private SensorManager mSensorManager;
-	//震动
-	private Vibrator vibrator;
 	int last;
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //获取传感器管理服务
-        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        //震动
-        vibrator = (Vibrator) getSystemService(Service.VIBRATOR_SERVICE);
         
         text = (TextView) findViewById(R.id.textview);
+        canteen = getResources().getStringArray(R.array.canteen);
         
-        text.setText("摇一摇~~");
+        final Vibrator vibe = (Vibrator)getSystemService(Context.VIBRATOR_SERVICE);
+		
+	    mShaker = new ShakeListener(this);
+	    mShaker.setOnShakeListener(new ShakeListener.OnShakeListener () {
+		    public void onShake() {
+		    	int r;
+		    	do {
+		    		r =  (int)(Math.random() * canteen.length);
+		    	} while(r==last);
+		    	last = r;
+		    	text.setText(canteen[r]);
+		    	vibe.vibrate(100);
+		    }
+	    });
     }
 
     @Override
@@ -44,90 +48,20 @@ public class ChiShaActivity extends Activity implements SensorEventListener {
     
     @Override
     protected void onResume(){
-    	super.onResume();
-        //加速度传感器
-        mSensorManager.registerListener(this, 
-        		mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), 
-        		SensorManager.SENSOR_DELAY_NORMAL);
-        //还有SENSOR_DELAY_UI、SENSOR_DELAY_FASTEST、SENSOR_DELAY_GAME等，根据不同应用，需要的反应速率不同，具体根据实际情况设定
-        text.setText("摇一摇~~");
+    	text.setText(R.string.welcome);
+		mShaker.onResume();
+		super.onResume();
     }
     @Override
     protected void onStop(){
-    	mSensorManager.unregisterListener(this);
-    	super.onStop();
+    	mShaker.onPause();
+	    super.onPause();
     }
 
     @Override
     protected void onPause(){
-    	mSensorManager.unregisterListener(this);
-    	super.onPause();
+    	mShaker.onStop();
+    	super.onStop();
     }
 
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-    	// TODO Auto-generated method stub
-    	//当传感器精度改变时回调该方法，Do nothing.
-    }
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-    	// TODO Auto-generated method stub
-    	int sensorType = event.sensor.getType();
-      
-        //values[0]:X轴，values[1]：Y轴，values[2]：Z轴
-	    float[] values = event.values;
-      
-        if(sensorType == Sensor.TYPE_ACCELEROMETER){
-      
-        	/**
-        	 * 因为一般正常情况下，任意轴数值最大就在9.8~10之间，只有在你突然摇动手机
-        	 * 的时候，瞬时加速度才会突然增大或减少。
-        	 * 所以，经过实际测试，只需监听任一轴的加速度大于14的时候，改变你需要的设置
-        	 * 就OK了~~~
-        	 */
-	       if((Math.abs(values[0])>14||Math.abs(values[1])>14||Math.abs(values[2])>14)){
-	       
-	    	   //摇动手机后，设置button上显示的字为空
-	    	   text.setText(getCanteen());
-	        
-	    	   //摇动手机后，再伴随震动提示~~
-	    	   vibrator.vibrate(500);
-	       
-	       }
-        }
-	}
-    
-    public String getCanteen(){
-    	int r;
-    	
-    	do {
-    		r =  (int)(Math.random() * 12 + 1);
-    	} while(r==last);
-    	last = r;
-    	
-    	switch(r) {
-    	case 1: 
-    		return "学一";
-    	case 2: 
-    		return "农园";
-    	case 3: 
-    		return "燕南";
-    	case 4: 
-    		return "艺园";
-    	case 5: 
-    		return "康西";
-    	case 6: 
-    		return "面食部";
-    	case 7: 
-    		return "松林";
-    	case 8: 
-    		return "康博斯饺子";
-    	case 9: 
-    		return "家园";
-    	case 10: 
-    		return "康中";
-    	default:
-    		return "小白房吧";
-    	}
-    }
 }
